5709c62aed6641a76a3133c4aeaf1f2a64e3428e
pratik mukherjee <headit@mlaitech.io>
2022-06-15 16:51:49 +0530

    adfs login

--- ABC/settings.py
+++ ABC/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
 AUTHENTICATION_BACKENDS = (
     'django_auth_adfs.backend.AdfsAuthCodeBackend',
 )
-#new key2
+#new key adfs
 AUTH_ADFS = {
     "SERVER": "login.adityabirlacapital.com",
     "CLIENT_ID": "cfe85d02-8677-48e6-8060-4444a812ccc0",
